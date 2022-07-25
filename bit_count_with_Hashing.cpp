e086cfb03b93a007c01c9acbd6b41dd034e61774
Shubham Badgujar <shubhambadgujar222@gmail.com>
2022-07-25 12:04:16 +0530

    bit count with Hashing

---
+++ bit_count_with_Hashing.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int Bits = 32;
+int Hash[Bits];
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int p;
+        cin >> p;
+        string bin = bitset<Bits>(p).to_string();
+        for (int i = 0; i < Bits; i++)
+        {
+            int bit = bin[i] - '0';
+            Hash[i] += bit;
+        }
+    }
+    cout << *max_element(begin(Hash), end(Hash)) << endl;
+
+    // Code Here
+    return 0;
+}
