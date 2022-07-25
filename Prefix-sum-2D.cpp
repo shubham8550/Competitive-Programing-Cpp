19c76549ab864f6bc86730245582ee6e80c3c298
Shubham Badgujar <shubhambadgujar222@gmail.com>
2022-07-25 13:55:55 +0530

    prefix sum 2D

---
+++ Prefix-sum-2D.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 1e3 + 10;
+int metrix[N][N];
+
+int main()
+{
+    int m;
+    cin >> m;
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            int p;
+            cin >> p;
+            metrix[i][j] = p + metrix[i - 1][j] + metrix[i][j - 1] - metrix[i - 1][j - 1];
+        }
+    }
+
+    int k;
+    cin >> k;
+
+    while (k--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int value = metrix[c][d] - metrix[a - 1][d] - metrix[c][b - 1] + metrix[a - 1][b - 1];
+        cout << value << endl;
+    }
+
+    return 0;
+}
